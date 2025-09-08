569db4c0197e120d680b36d277ab5fb87d3d84e2
ilovescience-1 <vitoria.ciseros@ucas-edu.net>
2025-09-08 13:06:47 -0600

    Ms larose

---
+++ Notes/expressions.py
@@ -0,0 +1,51 @@
+#VC 6th Expressions notes
+
+#what is an algorithm?
+    # a set of instructions to acomplish a task
+name = input("what is your name?")
+print("Hello", name)
+
+#algorithm for area
+    #is a set of instuctions to accomplish the task
+lenght = 5
+width = 20 
+area = lenght * width 
+print(lenght, "+", width, "=", area)
+
+#algorithm for area
+    #l piece of information is needed
+    #2 proccess the information
+    #3 provide an ouput
+
+age_1 = 14
+age_2 = 14
+age_3 = 14 
+age_4 = 15 
+total = age_1+ age_2+ age_3+ age_4 
+average = total/4
+print("the average age between:", age_1, "-", age_2, "-", age_3, "-", age_4, "-", average)
+
+#math equations!
+num_1 = input("Give me a number:\n")
+num_2 = input("Give me a number:\n")
+
+print("addition;", num_1 + num_2)
+print("subtraction", num_1 - num_2)
+print("multiplication", num_1 * num_2)
+print("division", num_1 / num_2)
+print("exponents", num_1 ** num_2)
+print("integer division", num_1 // num_2)
+print("modulo (remainder);", num_1 % num_2)
+print((3*5**2/15)-(5-2**2))
+
+#integers can hold positive and negative numbers but they have to be full numbers
+#a float is a decimal number
+#assignment opperator is a = sign or
+#you can add
+#you can also subtract
+#you can also devide
+#you can also multiply They just all have to be the same time everytime
+#for homework she wants us to write out the equation so they see it and also the results
+
+#if you set it up once you never have to do it again
+#
---
+++ variable_practice.py
@@ -0,0 +1,12 @@
+# VC 6th Variables Practice py
+name = "victoria"
+number = "2"
+number_2 = "87"
+what_you_had_for_breakfast_= "cereal"
+favorite_color = "red"
+school = "ucas"
+year = "2025"
+eye_color = "brown"
+age = "14"
+favorite_subject = "biology"
+print(name,number,number_2,what_you_had_for_breakfast_,favorite_color,school,year,eye_color,age,favorite_subject)
