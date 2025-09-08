8c3b5170e9c2e1b58dcd6697e0e7fa01fd0beb8f
ilovescience-1 <vitoria.ciseros@ucas-edu.net>
2025-09-08 13:44:27 -0600

    La rose

--- Notes/strings_notes.py
+++ Notes/strings_notes.py
@@ -1,1 +1,13 @@
-# VC 6th Stirngs Notes
+# VC 6th Strings Notes
+#strings are just a collection of letters held by quotation marks
+#examples of strings
+
+first_name = input("what is your name?:\n")
+
+last_name = input("what is your last name?:\n")
+
+full_name= first_name + " " + last_name
+
+sentence = "The quick brown fox jumps of over the lazy dog."
+
+print("welcome to my program", full_name, "" "!")
