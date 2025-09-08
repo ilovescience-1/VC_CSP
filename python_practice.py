239e60d14cad73aadc51bfec4117a0efa16c4793
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-09-08 19:01:50 +0000

    Ms LaRose

---
+++ python_practice.py
@@ -0,0 +1,6 @@
+#VC 6th First program python
+name = input ("what is your name ?")
+print("Hello " + name , "Nice to meet you" !)
+
+day_of_week = input ("what is your favorite day of the week ?")
+print(day_of_week + friday, "is my favorite day to !")
