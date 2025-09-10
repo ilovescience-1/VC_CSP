ee9ba01902aaa0c2a6b0d9614abc0219f61c2173
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-09-10 18:18:12 +0000

    changes to expressions pracitce

--- PRACTICES.py/expressions_practice.py
+++ PRACTICES.py/expressions_practice.py
@@ -3,5 +3,12 @@ num_2 = 24
 num_3 = 8
 num_4 = 4 
 num_5 = 6
+num_6 = 18
+num_7 = 3
+num_8 = 2
+num_9 = 5
+num_10 = 12
+num_11 = 
 math_equation = (num_1 - num_2 / num_3 * num_4 + num_5)
 print("the answer to the equation is:", math_equation)
+
