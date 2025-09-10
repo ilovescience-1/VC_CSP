6661d4e35a863c0a063995c81d2678c3965b65d7
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-09-10 21:32:18 +0000

    finehsed silly sentences

--- silly_sentences.py
+++ silly_sentences.py
@@ -1,4 +1,9 @@
 #VC 6th silly sentences
 
 name = input("Hello what is your name?")
-print("Hello" + name + "could you give me a one color and one animal and one food?")
+print("Hello" + name ")"
+color = input("could you give me a color?")
+animal = input("could you give me a animal?")
+food = input("could you give me a food?")
+room_in_a_house = input("could you give me a room in a house?")
+print("the" + color + animal + "went into the" + room_in_a_house + "and ate the" + food + "Then started dancing.")
