83685cfb950c11d195ab11bc85a414d2c1847473
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-09-15 21:12:12 +0000

    expresion practice finished

--- silly_sentences.py
+++ silly_sentences.py
@@ -1,9 +1,9 @@
 #VC 6th silly sentences
 
 name = input("Hello what is your name?")
-print("Hello" + name ")"
+print(("Hello") + name ")
 color = input("could you give me a color?")
 animal = input("could you give me a animal?")
 food = input("could you give me a food?")
 room_in_a_house = input("could you give me a room in a house?")
-print("the" + color + animal + "went into the" + room_in_a_house + "and ate the" + food + "Then started dancing.")
+print("the" + color + animal + "went into the" + room_in_a_house + "and ate the" + food + "Then started dancing." )
