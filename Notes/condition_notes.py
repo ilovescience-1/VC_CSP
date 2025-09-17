2f503e3c584bc0e18ab316bfe046fad8b2dd602e
ilovescience-1 <vitoria.ciseros@ucas-edu.net>
2025-09-17 13:42:41 -0600

    started condition notes

---
+++ Notes/condition_notes.py
@@ -0,0 +1,10 @@
+# VC 6th condition notes
+
+num = 12
+
+if num < 10:
+    print(f"{num} is a single digit number")
+if num > 100:
+    print(f"{num} is not a single digit number")
+if num == 4:
+    print("that is the winner number")
