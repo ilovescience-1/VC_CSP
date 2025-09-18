9820d420a6706a294ab86bc8d9e74cd11422a9a7
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-09-18 15:01:35 -0600

    Update old_enough.py redo

--- PRACTICES.py/old_enough.py
+++ PRACTICES.py/old_enough.py
@@ -3,7 +3,9 @@
 age = int(input("How old are you?"))
 if age >= 18:
     print("Awsome your old enough to vote!")
-
-age = int(input("How old are you?"))
-if age >= 15:
-    print("Cool your able to get your learners permit!")
+elif age >= 15:
+    print("You can get your learners permit")
+elif age >= 16:
+    print("Awesome your able to drive!")
+elif age >= 4:
+    print("Fun! your able to go to school now")
