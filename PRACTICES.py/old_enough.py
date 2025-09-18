9e805e76293b052208d1c4dc8783415c79205a53
ilovescience-1 <vitoria.ciseros@ucas-edu.net>
2025-09-18 13:44:17 -0600

    started the old enough practice

---
+++ PRACTICES.py/old_enough.py
@@ -0,0 +1,9 @@
+# VC 6th old enough practice
+
+age = int(input("How old are you?"))
+if age >= 18:
+    print("Awsome your old enough to vote!")
+
+age = int(input("How old are you?"))
+if age >= 15:
+    print("Cool your able to get your learners permit!")
