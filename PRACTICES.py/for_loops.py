04dd8b1ff8ed86e6c73fc96833e23769a0b81871
ilovescience-1 <vitoria.ciseros@ucas-edu.net>
2025-09-19 13:22:56 -0600

    started loops notes

---
+++ PRACTICES.py/for_loops.py
@@ -0,0 +1,14 @@
+# VC loops time
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+#print(f"The time in seconds since Jan 1, 1970)
+print(f"The time is: {current}")
+print(f"The hour is: {hour}")
+
+# Lists
+# They can always have any information as long as they are seprerated
+siblings = ["Mateo", "Valeria", "Violett", "Milan",]
