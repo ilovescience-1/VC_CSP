f93227fd402e5eb8266a8a0566b993b14d596778
ilovescience-1 <vitoria.ciseros@ucas-edu.net>
2025-09-19 13:43:30 -0600

    started time of day

---
+++ PRACTICES.py/time_of_day.py
@@ -0,0 +1,10 @@
+# VC 6th Time Of Day python practice
+
+time = int(input("What time is it?"))
+
+if time >=0:
+    print("Goodmorning")
+elif time <=12:
+    print("Good afternoon")
+elif time <=21:
+    print("Goodnight")
