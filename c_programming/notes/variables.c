5886195ecd93e388862c8700e79bcf484f9317d6
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-02 13:44:00 -0600

    everything

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,19 @@
+// VC 6th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A'; // will only hold 1 letter
+    char name[]= "victoria";
+   //bool passing = true;
+
+    printf("%d", num);
+
+    return 0;
+}
+// the semi colan and the int arent used in variables in python
+// what type of data variable holds
+// satic -> data types for variables don't change
+// you can't go back and change it
+// python its called dynamic
