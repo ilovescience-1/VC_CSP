3a941433be5d6f99b58512f68b01517c872c9f4c
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-03 13:42:39 -0600

    practice and variables practice

---
+++ c_programming/notes/practice_c_variables.c
@@ -0,0 +1,11 @@
+// VC 6th practice variables
+
+int main() {
+    char name[] = "alice";
+    int age = 25;
+
+    printf("name: %s\n", name);
+    printf("age: %d\n", age);
+    printf("%s is %d years old.\n", name , age);
+    return 0;
+}
--- c_programming/notes/variables.c
+++ c_programming/notes/variables.c
@@ -4,11 +4,21 @@
 int main(void){
     int num = 4;
     float pi = 3.14;
-    char grade = 'A'; // will only hold 1 letter
-    char name[]= "victoria";
+    char grade; // will only hold 1 letter
+    char name[20];
    //bool passing = true;
+    printf("what is your letter grade: ");
+    scanf("%c", &grade);
 
-    printf("%d", num);
+
+    printf("tell me a number: ");
+    scanf("%d", &num);
+    //this imput lets me get a space
+    printf("tell me your name: ");
+    fgets(name, sizeof(name), stdin);
+    
+    printf("%d\n", num);
+    printf("%s has a %c grade in the class!\n", name, grade);
 
     return 0;
 }
@@ -17,3 +27,21 @@ int main(void){
 // satic -> data types for variables don't change
 // you can't go back and change it
 // python its called dynamic
+//What is the main difference between declaring variables in Python and C?
+//in c we have to tell the computer what type the of data is being the variable
+//int 4 bytes-d
+//float 4 bytes--f
+//double 8 bytes-if
+//char 1 bytes-c
+//for a string.-s
+//you put those in when your doing the print statement
+// a string is a list of characters
+//comment you can use the //
+/*
+this is a comment
+*/
+//if you are using a single letter you use ''
+//if there is more characters you have to use
+// the & symbol is the same as = in python and adding it to the correct space
+// C wont let the user do the space because it will break the code
+//strings are multiple charachters
