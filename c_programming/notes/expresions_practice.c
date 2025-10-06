bc2bc73a71b9cf9ef1f664ce47cb866f4e2ef203
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-06 13:43:56 -0600

    expressions pratice

--- c_programming/notes/expresions_practice.c
+++ c_programming/notes/expresions_practice.c
@@ -3,7 +3,17 @@
 #include <math.h>
 
 int main(void){
-    int equation_1 = 7-24/8*4+6
-    
+    int equation_1 = 1;
+    int equation_2 = 9;
+    int equation_3 = 2;
+    int equation_4 = 26;
+    int equation_5 = -1;
+    int equation_6 = -3;
+    int equation_7 = 4;
+    int equation_8 = 23;
+    int equation_9 = 10;
+
+    printf("7-24/8*4+6 = %f\n,", (int) 7-24/8*4+6 =);
+
     return 0;
 }
