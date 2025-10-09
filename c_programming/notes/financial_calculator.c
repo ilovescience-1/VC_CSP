ee37e778bacd2203e06007aac747fb6b0562c152
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-09 19:43:44 +0000

    larose

---
+++ c_programming/notes/financial_calculator.c
@@ -0,0 +1,43 @@
+// VC 6th Financial Calculator
+
+int main(void) {
+    float income;
+    float rent;
+    float utilities;
+    float groceries;
+    float transportation;
+    float savings;
+    float spending_money;
+
+    printf("What is your monthly income: ");
+    scanf("%f", &income);
+    while (getchar() != '\n');
+
+    printf("What is your monthly rent/mortgage: ");
+    scanf("%f", &rent);
+    while (getchar() != '\n');
+
+    printf("What is your monthly utilities: ");
+    scanf("%f", &utilities);
+    while (getchar() != '\n');
+
+    printf("What is your monthly groceries: ");
+    scanf("%f", &groceries);
+    while (getchar() != '\n');
+
+    printf("What is your monthly transportation: ");
+    scanf("%f", &transportation);
+    while (getchar() != '\n');
+
+    savings = income * 0.10; 
+    spending_money = income - (rent + utilities + groceries + transportation + savings);
+
+    printf("\nYour rent is $%.2f and that is %.0f%% of your income.\n", rent, (rent / income) * 100);
+    printf("Your utilities are $%.2f and that is %.0f%% of your income.\n", utilities, (utilities / income) * 100);
+    printf("Your groceries are $%.2f and that is %.0f%% of your income.\n", groceries, (groceries / income) * 100);
+    printf("Your transportation is $%.2f and that is %.0f%% of your income.\n", transportation, (transportation / income) * 100);
+    printf("You should save $%.2f a month, that is 10%% of your income.\n", savings);
+    printf("You have $%.2f of spending money each month!\n", spending_money);
+
+    return 0;
+}
---
+++ c_programming/notes/variables_practice.c
@@ -0,0 +1,15 @@
+// VC 6th practice variables
+
+#include <stdio.h>
+int main() {
+    char name[] = "John Doe";
+    int lucky_number = 7;
+    int decimal_number = 342.5;
+    char breakfast[] = "Oatmeal with berries";
+    char favorite_color[] = "blue";
+    char school_name[] = "Springfield High School";
+    int current_year = 2025;
+    char eye_color[] = "brown";
+    int age = 16;
+    char favorite_subject[] = "Computer Science";
+}
