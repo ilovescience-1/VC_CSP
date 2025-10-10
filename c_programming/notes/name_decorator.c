bd970c85d548e4359a00fa7b8f1a135d4b063fdc
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-10 16:43:51 +0000

    started and finished name decorator

---
+++ c_programming/notes/name_decorator.c
@@ -0,0 +1,12 @@
+// VC 6th Name Decorator
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    printf("Hello what is your name?\n");
+    char name[20];
+    scanf("%s", name);)
+    printf("Hello, * %s, *!!!\n", name);
+    return 0;
+}
