591c2a3c9f93a5a98cf25b5fc1128b7a8c135244
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-13 18:13:47 +0000

    finished silly sentences

--- c_programming/notes/silly_sentences.c
+++ c_programming/notes/silly_sentences.c
@@ -4,5 +4,24 @@
 #include<string.h>
 
 int main(void){
-    
+    int main(void) {
+    char adjective[30];
+    char noun[30];
+    char verb[30];
+    printf("give an setting in a house: ");
+    scanf("%19s", setting in a house);
+    printf("give a color: ");
+    scanf("%19s", color);
+    printf("give me animal: ");
+    scanf("%19s", animal);
+    char story[200] = "The girl walked into the...";
+    strcat(story, setting in a house);
+    strcat(story, " with");
+    strcat(story, color);
+    strcat(story, " and she");
+    strcat(story, animal);
+    strcat(story, " at the pizza from the table");
+    printf("%s\n", story);
+
+    return 0;
 }
