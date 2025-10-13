3792c0b54c9a53f88b930470a5f3621ce39006b4
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-13 13:06:54 -0600

    started and fixed silly sentences

---
+++ c_programming/notes/loops_notes.c
@@ -0,0 +1,11 @@
+// VC 6th Loops notes
+#include<stdio.h>
+
+int main(void){
+    int nums[] = {4,243.553,34,23,65,43};
+
+    for(int x = 0; x < 11; x++){
+        printf("%d\n", nums[x]);
+    }
+    return 0;
+}
