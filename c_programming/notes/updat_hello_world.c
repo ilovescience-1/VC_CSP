040a1ea647bee08d8b38446ba69689d3fc173f26
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-14 21:15:40 +0000

    finished update hello world

--- c_programming/notes/updat_hello_world.c
+++ c_programming/notes/updat_hello_world.c
@@ -1,3 +1,11 @@
 // VC 6th Hello World Update
-
 #include<stdio.h>
+
+int main(void){
+    char* get_name(void)
+    char *name;
+    printf("What is your name: ");
+    printf("%s Hello!\n", name);
+    return name;
+    return 0;
+}
