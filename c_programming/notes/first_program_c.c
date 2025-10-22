04364fe80f84140c89c41504c5979482ed672be1
ilovescience-1 <victoria.cisneros@ucas-edu.net>
2025-10-22 13:39:00 -0600

    fixed a couple files

--- c_programming/notes/first_program_c.c
+++ c_programming/notes/first_program_c.c
@@ -1,12 +1,10 @@
 // VC 6th first program C
 
 #include <stdio.h>
-<<<<<<< HEAD
 int main(void) {
     char name[] = "please enter your name";
     printf("%s %s %s\n", "Hello", name, "welcome to your first C program!");
-=======
-
+}
 int main(void){
     char name [55];
     
@@ -15,6 +13,5 @@ int main(void){
     fgets(name, sizeof, (name), stdin);
     
     printf("Hello %s", name);
->>>>>>> 4e810e578465d51570296e9cf15d22fe87ba14e7
     return 0;
 }
